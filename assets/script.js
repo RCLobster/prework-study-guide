e8ee268778f8ce2ef3f12c995f8dc8076ba8ac52
RCLobster <44736025+RCLobster@users.noreply.github.com>
2023-12-01 17:26:16 -0800

    added JavaScript framework to application

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+
+/*if (topics === 'HTML') {
+
+    console.log("Let's Study HTML!");
+
+} else if (topics === 'CSS') {
+
+    console.log("Let's Study CSS!");
+
+} else if (topics === 'Git') {
+
+    console.log("Let's Study Git!");
+
+} else {
+
+    console.log('Please try again!');
+
+}*/
+
+
+for(var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+
+}
